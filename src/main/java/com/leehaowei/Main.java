83d38dbb89282eaa8f4222771fc00e2882cf6bb7
Haowei Lee <haoweilee.tpe@gmail.com>
2023-07-17 22:53:41 +0200

    feat: - add JPA read

--- src/main/java/com/leehaowei/Main.java
+++ src/main/java/com/leehaowei/Main.java
@@ -1,13 +1,40 @@
 package com.leehaowei;
 
+import com.leehaowei.customer.Customer;
+import com.leehaowei.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+
+        return args -> {
+            Customer alex = new Customer(
+                    "Alex",
+                    "alex@gmail.com",
+                    21
+            );
+
+            Customer jamila = new Customer(
+                    "Jamila",
+                    "alex@gmail.com",
+                    19
+            );
+
+            List<Customer> customers = List.of(alex, jamila);
+            customerRepository.saveAll(customers);
+        };
     }
 }
